b3ece3aed2890ac3e6662735e2652411e48b6790
TJ2005 <68985670+TJ2005@users.noreply.github.com>
2021-12-18 11:02:44 +0530

    Main.py
    This is the main code

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+from replit import db
+
+filt = ["PLS ENTER ALL THE WORDS TO BE BLACKLISTED HERE!!!!!!!"]
+bot = commands.Bot(command_prefix = ">>>")
+Channel_IDS = []
+db["Channels"]
+Channel_IDS = db["Channels"] 
+@bot.event
+async def on_ready():
+  print("logged in as "+str(bot.user))
+
+YES = ["Yes","yes","YES"]
+NO = ["No","no","NO"]
+
+@bot.event  
+async def on_guild_channel_create(channel):
+  #channel = discord.utils.get(channel.guild.channels, name='channel name')
+  if channel.name == "global-chat":
+    id = channel.id
+    Channel_IDS.append(id)
+    await channel.send("successfully set <#"+str(id)+"> as global chat")
+  id = channel.id
+  print (Channel_IDS)
+
+@bot.event
+async def on_guild_channel_delete(channel):
+  if channel.id in Channel_IDS:
+    Channel_IDS.remove(channel.id)
+  print(Channel_IDS)
+@bot.event
+async def on_message(message):
+  if message.author == bot.user:
+    return
+    print("bot msg")
+  else:
+    if message.channel.id in Channel_IDS:
+      print("message received"+str(message.content))
+      for chann in Channel_IDS:
+        if chann == message.channel.id or message.content in filt or "@everyone" in message.content or "@here" in message.content:
+          continue
+        await bot.get_channel(chann).send("**"+str(message.author)+"**  :globe_with_meridians: " + str(message.content))
+        print("message sent" +str(message.content))
+
+db["Channels"] = Channel_IDS
+bot.run("Token")
